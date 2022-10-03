a90510efeeaa30e66a930c6cfb367b09d9963077
‘qianxi’ <3112560244@qq.com>
2022-10-03 22:01:40 +0800

    jwt拦截 优化接口 创建token效验和拦截注入 修改request配置文件至vue.utils

--- src/main.js
+++ src/main.js
@@ -4,7 +4,8 @@ import router from './router'
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 import './assets/gloable.css'
-import request from "../../src/main/java/com/qx/demo/utils/request";
+// import request from "../../src/main/java/com/qx/demo/utils/request";
+import request from "../src/utils/request"
 
 Vue.config.productionTip = false
 
